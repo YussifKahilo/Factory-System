dda7fe3e672c111d86139ffdaf710d4a2da1a247
mostafahassan071 <mh1687919@gmail.com>
2019-11-24 17:29:22 +0200

    address

---
+++ address.java
@@ -0,0 +1,58 @@
+
+package factroy.system;
+public class address {
+    
+    private  int houseNumber ;
+    private  int flateNumber ;
+    private  String  streetName ;
+    private String place ;
+
+    public address() {
+    }
+
+    public address(int houseNumber, int flateNumber, String streetName,
+            String place) {
+        this.houseNumber = houseNumber;
+        this.flateNumber = flateNumber;
+        this.streetName = streetName;
+        this.place = place;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public void setFlateNumber(int flateNumber) {
+        this.flateNumber = flateNumber;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public int getFlateNumber() {
+        return flateNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String toString() {
+        return  + houseNumber + " " + flateNumber + "  " + streetName + " " 
+                + place ;
+    }
+        
+}
