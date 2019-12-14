cbc67876dea095103ed0b40381ba9c9c2c33ae02
ECaAmiNOo <56687866+ECaAmiNOo@users.noreply.github.com>
2019-12-14 23:36:46 +0200

    updates in Employee , Facotry & Manager classes

--- Factory.java
+++ Factory.java
@@ -3,127 +3,147 @@ import java.util.ArrayList;
 
 public class Factory {
 
-    private ArrayList<Worker> workers ;
-    private ArrayList<SuperVisor> superVisors;
-    private ArrayList<SalesMan> salesMen;
-    private Manager manager;
-    private Storage storage;
-    private Financial financial;
-    private FactorySystem factorySystem;
+	private ArrayList<Worker> workers;
+	private ArrayList<SuperVisor> superVisors;
+	private ArrayList<SalesMan> salesMen;
+	private Manager manager;
+	private Storage storage;
+	private Financial financial;
+	private FactorySystem factorySystem;
 
-    public Factory() {
-        manager = new Manager();
-        workers = new ArrayList<>();
-        superVisors = new ArrayList<>();
-        salesMen = new ArrayList<>();
-        storage = new Storage();
-        financial = new Financial();
-        setWorkers(this.workers, "Workers");
-        setSuperVisors(this.superVisors, "SuperVisors");
-        setSalesMen(this.salesMen, "SalesMen");
-        this.factorySystem = new FactorySystem(workers, superVisors, salesMen, manager);
-    }
+	public Factory() {
+		manager = new Manager();
+		workers = new ArrayList<>();
+		superVisors = new ArrayList<>();
+		salesMen = new ArrayList<>();
+		storage = new Storage();
+		financial = new Financial();
+		setWorkers(this.workers, "Workers");
+		setSuperVisors(this.superVisors, "SuperVisors");
+		setSalesMen(this.salesMen, "SalesMen");
+		this.factorySystem = new FactorySystem(workers, superVisors, salesMen, manager);
+	}
 
-    public FactorySystem getFactorySystem() {
-        return factorySystem;
-    }
+	public FactorySystem getFactorySystem() {
+		return factorySystem;
+	}
 
-    public void setWorkers(ArrayList<Worker> persons, String fileName) {
-        ArrayList<String> LINES = new ArrayList<String>();
-        FileData.getData(LINES, fileName + ".txt");
-        for (int i = 0; i < LINES.size(); i++) {
-            String line = LINES.get(i);
-            String[] Line = line.split("#");
-            Worker person = new Worker(new Employee(Line[0], Line[1], Line[2], Double.parseDouble(Line[3]),
-                    Double.parseDouble(Line[4]), Double.parseDouble(Line[5]), Double.parseDouble(Line[6]), Line[7],
-                    Line[8], Line[9],
-                    new Address(Integer.parseInt(Line[10]), Integer.parseInt(Line[11]), Integer.parseInt(Line[12]),
-                            Line[13], Line[14], Line[15]),
-                    new BankAccount(Line[16], Double.parseDouble(Line[17])),
-                    new BirthDate(Integer.parseInt(Line[18]), Integer.parseInt(Line[19]), Integer.parseInt(Line[20]))));
-            persons.add(person);
-        }
-    }
-    
-    public void setSuperVisors(ArrayList<SuperVisor> persons, String fileName) {
-        ArrayList<String> LINES = new ArrayList<String>();
-        FileData.getData(LINES, fileName + ".txt");
-        ArrayList <Employee> worker = new ArrayList<Employee>();
-        for (int i = 0; i < LINES.size(); i++) {
-            String line = LINES.get(i);
-            String[] Line = line.split("#");
-            SuperVisor person = new SuperVisor(new Employee(Line[0], Line[1], Line[2], Double.parseDouble(Line[3]),
-                    Double.parseDouble(Line[4]), Double.parseDouble(Line[5]), Double.parseDouble(Line[6]), Line[7],
-                    Line[8], Line[9],
-                    new Address(Integer.parseInt(Line[10]), Integer.parseInt(Line[11]), Integer.parseInt(Line[12]),
-                            Line[13], Line[14], Line[15]),
-                    new BankAccount(Line[16], Double.parseDouble(Line[17])),
-                    new BirthDate(Integer.parseInt(Line[18]), Integer.parseInt(Line[19]), Integer.parseInt(Line[20]))),workers);
-            persons.add(person);
-        }
-    }
-    
-    public void setSalesMen(ArrayList<SalesMan> persons, String fileName) {
-        ArrayList<String> LINES = new ArrayList<String>();
-        FileData.getData(LINES, fileName + ".txt");
-        for (int i = 0; i < LINES.size(); i++) {
-            String line = LINES.get(i);
-            String[] Line = line.split("#");
-            SalesMan person = new SalesMan(new Employee(Line[0], Line[1], Line[2], Double.parseDouble(Line[3]),
-                    Double.parseDouble(Line[4]), Double.parseDouble(Line[5]), Double.parseDouble(Line[6]), Line[7],
-                    Line[8], Line[9],
-                    new Address(Integer.parseInt(Line[10]), Integer.parseInt(Line[11]), Integer.parseInt(Line[12]),
-                            Line[13], Line[14], Line[15]),
-                    new BankAccount(Line[16], Double.parseDouble(Line[17])),
-                    new BirthDate(Integer.parseInt(Line[18]), Integer.parseInt(Line[19]), Integer.parseInt(Line[20]))));
-            persons.add(person);
-        }
-    }
+	public void setWorkers(ArrayList<Worker> persons, String fileName) {
+		ArrayList<String> LINES = new ArrayList<String>();
+		FileData.getData(LINES, fileName + ".txt");
+		for (int i = 0; i < LINES.size(); i++) {
+			String line = LINES.get(i);
+			String[] Line = line.split("#");
+			Worker person = new Worker(new Employee(Line[0], Line[1], Line[2], Double.parseDouble(Line[3]),
+					Double.parseDouble(Line[4]), Double.parseDouble(Line[5]), Double.parseDouble(Line[6]), Line[7],
+					Line[8], Line[9],
+					new Address(Integer.parseInt(Line[10]), Integer.parseInt(Line[11]), Integer.parseInt(Line[12]),
+							Line[13], Line[14], Line[15]),
+					new BankAccount(Line[16], Double.parseDouble(Line[17])),
+					new BirthDate(Integer.parseInt(Line[18]), Integer.parseInt(Line[19]), Integer.parseInt(Line[20]))));
+			persons.add(person);
+		}
+	}
 
-    public void addWorker(Worker worker) {
-        factorySystem.addUser(worker);
-        workers.add(worker);
-    }
+	public void setSuperVisors(ArrayList<SuperVisor> persons, String fileName) {
+		ArrayList<String> LINES = new ArrayList<String>();
+		FileData.getData(LINES, fileName + ".txt");
+		ArrayList<Employee> worker = new ArrayList<Employee>();
+		for (int i = 0; i < LINES.size(); i++) {
+			String line = LINES.get(i);
+			String[] Line = line.split("#");
+			SuperVisor person = new SuperVisor(new Employee(Line[0], Line[1], Line[2], Double.parseDouble(Line[3]),
+					Double.parseDouble(Line[4]), Double.parseDouble(Line[5]), Double.parseDouble(Line[6]), Line[7],
+					Line[8], Line[9],
+					new Address(Integer.parseInt(Line[10]), Integer.parseInt(Line[11]), Integer.parseInt(Line[12]),
+							Line[13], Line[14], Line[15]),
+					new BankAccount(Line[16], Double.parseDouble(Line[17])),
+					new BirthDate(Integer.parseInt(Line[18]), Integer.parseInt(Line[19]), Integer.parseInt(Line[20]))),
+					workers);
+			persons.add(person);
+		}
+	}
 
-    public void removeWorker(Worker worker) {
-        factorySystem.removeUser(worker);
-        workers.remove(worker);
-    }
+	public void setSalesMen(ArrayList<SalesMan> persons, String fileName) {
+		ArrayList<String> LINES = new ArrayList<String>();
+		FileData.getData(LINES, fileName + ".txt");
+		for (int i = 0; i < LINES.size(); i++) {
+			String line = LINES.get(i);
+			String[] Line = line.split("#");
+			SalesMan person = new SalesMan(new Employee(Line[0], Line[1], Line[2], Double.parseDouble(Line[3]),
+					Double.parseDouble(Line[4]), Double.parseDouble(Line[5]), Double.parseDouble(Line[6]), Line[7],
+					Line[8], Line[9],
+					new Address(Integer.parseInt(Line[10]), Integer.parseInt(Line[11]), Integer.parseInt(Line[12]),
+							Line[13], Line[14], Line[15]),
+					new BankAccount(Line[16], Double.parseDouble(Line[17])),
+					new BirthDate(Integer.parseInt(Line[18]), Integer.parseInt(Line[19]), Integer.parseInt(Line[20]))));
+			persons.add(person);
+		}
+	}
 
-    public void addSuperVisor(SuperVisor superVisor) {
-        factorySystem.addUser(superVisor);
-        superVisors.add(superVisor);
-    }
+	public void removeEmployee(Employee employee) {
+		ArrayList<String> bankAccount = new ArrayList<String>();
+		
+		FileData.getData(bankAccount,"BankAccount.txt");
+		bankAccount.remove(employee.getBankAccount().getAccountNumber());
+		FileData.setData(bankAccount, "BankAccount.txt");
+		if (employee.getId().startsWith("2")) {
+			factorySystem.removeUser(employee);
+			workers.remove(employee);
+		} else if (employee.getId().startsWith("4")) {
+			factorySystem.removeUser(employee);
+			salesMen.remove(employee);
+		} else if (employee.getId().startsWith("3")) {
+			factorySystem.removeUser(employee);
+			superVisors.remove(employee);
+		}
 
-    public void removeSuperVisor(SuperVisor superVisor) {
-        factorySystem.removeUser(superVisor);
-        superVisors.remove(superVisor);
-    }
+	}
+	public void assignWokersToSupervisors() {
+		ArrayList<String> idsOfWorkers = new ArrayList<String>();
+		String str = "";
+		int totalNumberOfWorkers = workers.size();
+		int totalNumberOfSuperVisors=superVisors.size();
+		if(totalNumberOfWorkers%totalNumberOfSuperVisors==0) {
+			int numberOfWorkersForEach =totalNumberOfWorkers/totalNumberOfSuperVisors;
+			for(int i =0;i<totalNumberOfWorkers;i++) {
+				if((i+1)%numberOfWorkersForEach!=0) {
+					str += workers.get(i).getId() + "#";
+				}
+				idsOfWorkers.add(str);
+			}
+		}
+		
+	}
+	public void addWorker(Worker worker) {
+		factorySystem.addUser(worker);
+		workers.add(worker);
+	}
 
-    public void removeSalesMan(SalesMan salesMan) {
-        factorySystem.removeUser(salesMan);
-        salesMen.remove(salesMan);
-    }
+	public void addSuperVisor(SuperVisor superVisor) {
+		factorySystem.addUser(superVisor);
+		superVisors.add(superVisor);
+	}
 
-    public void addSalesMan(SalesMan salesMan) {
-        factorySystem.addUser(salesMan);
-        salesMen.add(salesMan);
-    }
+	public void addSalesMan(SalesMan salesMan) {
+		factorySystem.addUser(salesMan);
+		salesMen.add(salesMan);
+	}
 
-    public ArrayList<SuperVisor> getSuperVisors() {
-        return superVisors;
-    }
+	public ArrayList<SuperVisor> getSuperVisors() {
+		return superVisors;
+	}
 
-    public ArrayList<SalesMan> getSalesMen() {
-        return salesMen;
-    }
+	public ArrayList<SalesMan> getSalesMen() {
+		return salesMen;
+	}
 
-    public ArrayList<Worker> getWorkers() {
-        return workers;
-    }
+	public ArrayList<Worker> getWorkers() {
+		return workers;
+	}
 
-    public void startUserInterface(Person user){
-        factorySystem.userUtility(user, manager, workers, superVisors, salesMen, financial, storage, this);
-    }
+	public void startUserInterface(Person user) {
+		factorySystem.userUtility(user, manager, workers, superVisors, salesMen, financial, storage, this);
+	}
 
 }
--- Manager.java
+++ Manager.java
@@ -5,135 +5,205 @@ import java.util.Scanner;
 
 public class Manager extends Person {
 
-    public Manager() {
-        super("1100", "Kahilo", "00");
-    }
+	public Manager() {
+		super("1100", "Kahilo", "00");
+	}
 
-    public void promote(Employee employee, String promotedEmployee, Factory factory) {
-        employee.setOverallRate(0);
-        if (promotedEmployee.equalsIgnoreCase("SuperVisor")) {
-            factory.removeWorker(employee);
-            employee.setId("33" + employee.getId().substring(2));
-            factory.addSuperVisor(employee);
-        } else {
-            factory.removeWorker(employee);
-            employee.setId("44" + employee.getId().substring(2));
-            factory.addSalesMan(employee);
-        }
+	
+	public void promote(Employee employee, String promotedEmployee, Factory factory) {
+		employee.setOverallRate(0);
+		if (promotedEmployee.equalsIgnoreCase("SuperVisor")) {
 
-    }
+			factory.removeEmployee((Worker) employee);
+			employee.setId("33" + employee.getId().substring(2));
+			factory.addSuperVisor((SuperVisor) employee);
+			ArrayList<String> superVisors = new ArrayList<String>();
+			for (int i = 0; i < factory.getSuperVisors().size(); i++) {
+				superVisors.add(factory.getSuperVisors().get(i).toString());
+			}
+			FileData.setData(superVisors, "SuperVisors.txt");
+		} else {
+			ArrayList<String> salesMen = new ArrayList<String>();
+			employee.setId("44" + employee.getId().substring(2));
+			factory.removeEmployee(employee);
+			factory.addSalesMan((SalesMan) employee);
+			for (int i = 0; i < factory.getSuperVisors().size(); i++) {
+				salesMen.add(factory.getSalesMen().get(i).toString());
+			}
+			FileData.setData(salesMen, "SuperVisors.txt");
 
-    public Employee searchForEmployee(String id, ArrayList<Employee> employees) {
-        boolean employeeIsFound = false;
-        Employee chosenEmployee = null;
-        for (int i = 0; i < employees.size() && !employeeIsFound; i++) {
-            if (id.equals(employees.get(i).getId())) {
-                employeeIsFound = true;
-                chosenEmployee = employees.get(i);
-            }
-        }
-        return chosenEmployee;
-    }
+		}
 
-    public void showEmployees(ArrayList<Employee> persons) {
+	}
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Do you Want to sort employees By thier ID OR Over All Rate ?\n1)ID\n2)Over All Rate");
-        int sortingChoice = input.nextInt();
-        System.out.println(
-                ".____________________________________________________________________________________________________________________________________.");
-        System.out.println(
-                "|   ID   |       Name        |  Overall Rate  |      Salary      |  Phone Number  | Hiring Date |               E-Mail               |");
-        System.out.println(
-                "+--------+-------------------+----------------+------------------+----------------+-------------+------------------------------------+");
-        int j;
-        int indexOfMax = 0;
-        if (sortingChoice == 2) {
-            for (int k = 0; k < persons.size(); k++) {
-                double max = persons.get(k).getOverallRate();
-                for (j = k; j < persons.size(); j++) {
-                    if (max < persons.get(j).getOverallRate()) {
-                        max = persons.get(j).getOverallRate();
-                        indexOfMax = j;
-                    }
-                }
-                Collections.swap(persons, indexOfMax, k);
-            }
-        }
-        for (int i = 0; i < persons.size(); i++) {
-            System.out.print("|");
-            String[] Name = persons.get(i).getName().split(" ");
-            String name = " " + Name[0];
-            String space = "", space_for_email = "";
-            for (int k = 0; k < 19 - name.length(); k++) {
-                space += " ";
-            }
-            name = name.concat(space);
+	public Employee searchForEmployee(String id, ArrayList<Employee> employees) {
+		boolean employeeIsFound = false;
+		Employee chosenEmployee = null;
+		for (int i = 0; i < employees.size() && !employeeIsFound; i++) {
+			if (id.equals(employees.get(i).getId())) {
+				employeeIsFound = true;
+				chosenEmployee = employees.get(i);
+			}
+		}
+		return chosenEmployee;
+	}
 
-            for (j = 0; j < 35 - persons.get(i).getEmail().length(); j++) {
-                space_for_email += " ";
-            }
-            String email = " " + persons.get(i).getEmail() + space_for_email;
+	public void showEmployees(int table_option, ArrayList<Employee> persons) {
+		ArrayList<Employee> employee = new ArrayList<Employee>();
+		if (table_option == 1) {
+			for (int i = 0; i < persons.size(); i++) {
+				if (persons.get(i).getId().startsWith("2")) {
+					employee.add(persons.get(i));
+				}
+			}
+		} else if (table_option == 2) {
+			for (int i = 0; i < persons.size(); i++) {
+				if (persons.get(i).getId().startsWith("3")) {
+					employee.add(persons.get(i));
+				}
+			}
+		} else {
+			for (int i = 0; i < persons.size(); i++) {
+				if (persons.get(i).getId().startsWith("4")) {
+					employee.add(persons.get(i));
+				}
+			}
+		}
+		Scanner input = new Scanner(System.in);
+		System.out.println("Do you Want to sort employees By thier ID OR Over All Rate ?\n1)ID\n2)Over All Rate");
+		int sortingChoice = input.nextInt();
+		System.out.println(
+				".____________________________________________________________________________________________________________________________________.");
+		System.out.println(
+				"|   ID   |       Name        |  Overall Rate  |      Salary      |  Phone Number  | Hiring Date |               E-Mail               |");
+		System.out.println(
+				"+--------+-------------------+----------------+------------------+----------------+-------------+------------------------------------+");
+		int j;
+		int indexOfMax = 0;
+		if (sortingChoice == 2) {
+			for (int k = 0; k < employee.size(); k++) {
+				double max = employee.get(k).getOverallRate();
+				for (j = k; j < employee.size(); j++) {
+					if (max < employee.get(j).getOverallRate()) {
+						max = employee.get(j).getOverallRate();
+						indexOfMax = j;
+					}
+				}
+				Collections.swap(employee, indexOfMax, k);
+			}
+		}
+		for (int i = 0; i < employee.size(); i++) {
+			System.out.print("|");
+			String[] Name = employee.get(i).getName().split(" ");
+			String name = " " + Name[0];
+			String space = "", space_for_email = "";
+			for (int k = 0; k < 19 - name.length(); k++) {
+				space += " ";
+			}
+			name = name.concat(space);
 
-            System.out.printf("%7s%2s%19s%1s%11s%6s%13.2f%6s%15s%2s%11s%3s%35s%1s", persons.get(i).getId(), "|", name,
-                    "|", (int) persons.get(i).getOverallRate() + " / 10", "|", persons.get(i).getSalary(), "|",
-                    "+02" + persons.get(i).getPhoneNumber(), "|", persons.get(i).getHiringDate(), "|", email, "|");
-            System.out.println("");
-        }
-        System.out.println(
-                "+------------------------------------------------------------------------------------------------------------------------------------+");
-    }
+			for (j = 0; j < 35 - employee.get(i).getEmail().length(); j++) {
+				space_for_email += " ";
+			}
+			String email = " " + employee.get(i).getEmail() + space_for_email;
 
-    public void storageManagment(int num, Storage storage) {
-        Scanner input = new Scanner(System.in);
-        if (num == 1) {
-            System.out.println("The number of stored goods: " + storage.getNumberOfStoredGoods());
-        } else if (num == 2) {
-            System.out.println("The number of goods that have been manufactured this month: "
-                    + storage.getnumberOfGoodsThisMonth());
-        } else if (num == 3) {
-            Financial financial = new Financial();
-            System.out.println("The number of sold goods:" + storage.getNumberOfSoldGood());
-            financial.setProfit(
-                    financial.getMatrialsPrice() + (storage.getNumberOfSoldGood() * storage.getPriceofGoods()));
-            financial.setTotalMoney(financial.getTotalMoney() + financial.getProfit());
-        } else if (num == 4) {
-            System.out.println("Enter The Number Of Goods To be Sold: ");
-            String numOfGoodsToBeSold = input.next();
-            storage.setNumberOfGoodsTobeSold(Integer.parseInt(numOfGoodsToBeSold));
-            System.out.println("Enter The price of each goods");
-            String priceOfGoodsToBeSold = input.next();
-            storage.setPriceOfGoods(Double.parseDouble(priceOfGoodsToBeSold));
-        }
-    }
+			System.out.printf("%7s%2s%19s%1s%11s%6s%13.2f%6s%15s%2s%11s%3s%35s%1s", employee.get(i).getId(), "|", name,
+					"|", (int) employee.get(i).getOverallRate() + " / 10", "|", employee.get(i).getSalary(), "|",
+					"+02" + employee.get(i).getPhoneNumber(), "|", employee.get(i).getHiringDate(), "|", email, "|");
+			System.out.println("");
+		}
+		System.out.println(
+				"+------------------------------------------------------------------------------------------------------------------------------------+");
+	}
 
-    public void financialManagment(int num, Financial financial) {
-        Scanner input = new Scanner(System.in);
-        if (num == 1) {
-            System.out.println("The total money is : " + financial.getTotalMoney());
-        } else if (num == 2) {
-            System.out.println("profits is : " + financial.getProfit());
-        } else if (num == 3) {
-            System.out.println("Enter the price of materials you bought :");
-            String priceOfTheMaterials = input.next();
-            financial.setMatrialsPrice(Double.parseDouble(priceOfTheMaterials) * (-1));
-            financial.setTotalMoney(financial.getTotalMoney() + financial.getMatrialsPrice());
-        }
-    }
+	public void storageManagment(int num, Storage storage) {
+		Scanner input = new Scanner(System.in);
+		if (num == 1) {
+			System.out.println("The number of stored goods: " + storage.getNumberOfStoredGoods());
+		} else if (num == 2) {
+			System.out.println("The number of goods that have been manufactured this month: "
+					+ storage.getnumberOfGoodsThisMonth());
+		} else if (num == 3) {
+			Financial financial = new Financial();
+			System.out.println("The number of sold goods:" + storage.getNumberOfSoldGood());
+			financial.setProfit(
+					financial.getMatrialsPrice() + (storage.getNumberOfSoldGood() * storage.getPriceofGoods()));
+			financial.setTotalMoney(financial.getTotalMoney() + financial.getProfit());
+		} else if (num == 4) {
+			System.out.println("Enter The Number Of Goods To be Sold: ");
+			String numOfGoodsToBeSold = input.next();
+			storage.setNumberOfGoodsTobeSold(Integer.parseInt(numOfGoodsToBeSold));
+			System.out.println("Enter The price of each goods");
+			String priceOfGoodsToBeSold = input.next();
+			storage.setPriceOfGoods(Double.parseDouble(priceOfGoodsToBeSold));
+		}
+	}
 
-    public void setTarget(int target, int numberOfWorkers) {
-        ArrayList<String> arrayListOfTarget = new ArrayList<String>();
-        arrayListOfTarget.add(target + "#" + (target / numberOfWorkers));
-        FileData.setData(arrayListOfTarget, "Target.txt");
-    }
+	public void financialManagment(int num, Financial financial) {
+		Scanner input = new Scanner(System.in);
+		if (num == 1) {
+			System.out.println("The total money is : " + financial.getTotalMoney());
+		} else if (num == 2) {
+			System.out.println("profits is : " + financial.getProfit());
+		} else if (num == 3) {
+			System.out.println("Enter the price of materials you bought :");
+			String priceOfTheMaterials = input.next();
+			financial.setMatrialsPrice(Double.parseDouble(priceOfTheMaterials) * (-1));
+			financial.setTotalMoney(financial.getTotalMoney() + financial.getMatrialsPrice());
+		}
+	}
 
-    public void firingEmployee(Employee employee, Factory factory) {
-        if (employee.getId().startsWith("22")) {
-            factory.removeWorker(employee);
-        } else if (employee.getId().startsWith("33")) {
-            factory.removeSalesMan(employee);
-        } else if (employee.getId().startsWith("44")){
-            factory.removeSuperVisor(employee);
-        }
-    }
+	public void setTarget(int target, int numberOfWorkers) {
+		ArrayList<String> arrayListOfTarget = new ArrayList<String>();
+		arrayListOfTarget.add(target + "#" + (target / numberOfWorkers));
+		FileData.setData(arrayListOfTarget, "Target.txt");
+	}
+
+	public void firingEmployee(Employee employee, Factory factory) {
+		factory.removeEmployee(employee);
+	}
+
+	public void giveSalary(ArrayList<Employee> employees) {
+		for (int i = 0; i < employees.size(); i++) {
+			employees.get(i).getBankAccount().setBalance(employees.get(i).getSalary() + employees.get(i).getBonus()
+					+ employees.get(i).getBankAccount().getBalance());
+		}
+	}
+
+	public Employee searchForWorker(String id, ArrayList<Worker> workers) {
+		Employee chosenEmployee = null;
+
+		for (int i = 0; i < workers.size(); i++) {
+			if (workers.get(i).getId().equals(id)) {
+				chosenEmployee = workers.get(i);
+				i = workers.size();
+			}
+		}
+		return chosenEmployee;
+
+	}
+
+	public Employee searchForSuperVisor(String id, ArrayList<SuperVisor> superVisors) {
+		Employee chosenEmployee = null;
+
+		for (int i = 0; i < superVisors.size(); i++) {
+			if (superVisors.get(i).getId().equals(id)) {
+				chosenEmployee = superVisors.get(i);
+				i = superVisors.size();
+			}
+		}
+		return chosenEmployee;
+	}
+
+	public Employee searchSalesMan(String id, ArrayList<SalesMan> salesMen) {
+		Employee chosenEmployee = null;
+
+		for (int i = 0; i < salesMen.size(); i++) {
+			if (salesMen.get(i).getId().equals(id)) {
+				chosenEmployee = salesMen.get(i);
+				i = salesMen.size();
+			}
+		}
+		return chosenEmployee;
+	}
 }
